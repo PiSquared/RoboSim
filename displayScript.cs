4e374d72c033db8625bc029e4b7d9f2488cd43f4
navyassassin <45081071+navyassassin@users.noreply.github.com>
2019-02-27 12:02:01 -0500

    Add files via upload

---
+++ displayScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displayScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var devices : WebCamDevice[] = WebCamTexture.devices;
+        for (var i = 0; i < devices.length; i++)
+            Debug.Log(devices[i].name);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        WebCamTexture webcam = WebCamTexture("NameOfDevice");
+        renderer.material.mainTexture = webcam;
+        webcam.Play();
+
+    }
+}
